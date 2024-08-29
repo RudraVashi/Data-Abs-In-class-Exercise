e8d4b75eaae94102c601cbe78e59e6c806b71bee
RudraVashi <115006367+RudraVashi@users.noreply.github.com>
2024-08-29 17:13:52 -0500

    Add files via upload
    8/29/24 In class activities

---
+++ Multiplesof5and7.java
@@ -0,0 +1,13 @@
+package ClassExercises;
+public class Multiplesof5and7{
+    public static int sumOf(int n){
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            if (i % 5 == 0 || i % 7 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
+
---
+++ arrayNums.java
@@ -0,0 +1,20 @@
+package ClassExercises;
+
+public class arrayNums {
+    public static boolean ArrayNums(int[] nums){
+        for(int i =0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }   
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        arrayNums cd = new arrayNums();
+        int[] nums = {1, 2, 3, 0};
+        System.out.println(cd.ArrayNums(nums));  // Output: true
+    }
+}
