67e4547c44fae98976ca9b9445e4110043d85ce7
RudraVashi <rvashi@trinity.edu>
2024-09-05 12:11:55 -0500

    9/5/24 In Class Activity

---
+++ parenthesesMatching.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class parenthesesMatching {
+    public boolean isValid(String s){
+        if(s.length() <= 1){
+            return false;
+        }    
+        if(s.length() % 2 !=0){
+            return false;
+        }
+
+        Stack <Character> check = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                check.push(s.charAt(i));
+            }
+            else{
+                if(s.charAt(i)== ')'){
+                    if(check.pop()!= '('){
+                        return false;
+                    }
+                }
+                if(s.charAt(i)== ']'){
+                    if(check.pop()!= '['){
+                        return false;
+                    }
+                }
+                if(s.charAt(i)== '}'){
+                    if(check.pop()!= '{'){
+                        return false;
+                    }
+                }
+                if(check.isEmpty() == false){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
